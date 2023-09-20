5724575914ea01c7f0f73a60e3fe98dea0cda989
WukoSiric <lstainer2002@gmail.com>
2023-09-20 19:31:39 +0930

    AggregationServer takes in port arg

--- AggregationServer.java
+++ AggregationServer.java
@@ -1,3 +1,13 @@
+import java.net.ServerSocket;
+
 public class AggregationServer {
+    private AggregationServer() {}
     
+    private ServerSocket serverSocket; // Server socket to which the clients connect
+    
+    public static void main(String[] args) {
+        int port = args.length > 0 ? Integer.parseInt(args[0]) : 4567;
+        System.out.println("Starting server on port " + port);
+
+    }
 }
