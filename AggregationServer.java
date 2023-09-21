a17863dbbcb61f9117675ccc74feb11038821655
WukoSiric <lstainer2002@gmail.com>
2023-09-21 22:28:31 +0930

    Remove redundant server socket

--- AggregationServer.java
+++ AggregationServer.java
@@ -4,8 +4,6 @@ import java.net.*;
 public class AggregationServer {
     private AggregationServer() {}
     
-    private ServerSocket serverSocket; // Server socket to which the clients connect
-    
     public static void main(String[] args) {
         int port = args.length > 0 ? Integer.parseInt(args[0]) : 4567;
         System.out.println("Starting server on port " + port);
