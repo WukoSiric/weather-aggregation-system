63e2792d62d50b5bd08df69f25b83e997e696e48
WukoSiric <lstainer2002@gmail.com>
2023-09-21 21:36:21 +0930

    Basic communication established

--- AggregationServer.java
+++ AggregationServer.java
@@ -1,4 +1,5 @@
-import java.net.ServerSocket;
+import java.io.*; 
+import java.net.*; 
 
 public class AggregationServer {
     private AggregationServer() {}
@@ -8,6 +9,16 @@ public class AggregationServer {
     public static void main(String[] args) {
         int port = args.length > 0 ? Integer.parseInt(args[0]) : 4567;
         System.out.println("Starting server on port " + port);
-
+        
+        try {
+            ServerSocket ss = new ServerSocket(port); 
+            Socket s = ss.accept();
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            String str = (String)dis.readUTF();
+            System.out.println("Message: " + str);
+            ss.close(); 
+        } catch(Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
--- ContentServer.java
+++ ContentServer.java
@@ -1,3 +1,24 @@
+import java.io.*; 
+import java.net.*; 
+
 public class ContentServer {
-    
+    private ContentServer() {}
+
+    public static void main(String[] args) {
+        if (args.length != 1) { 
+            System.err.println("Usage: java ContentServer <port number>"); 
+            System.exit(1); 
+        }
+        int port = Integer.parseInt(args[0]);
+        try {
+            Socket s = new Socket("localhost", port);
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            out.writeUTF("Hello, server!");
+            out.flush();
+            out.close();
+            s.close();
+        } catch(Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
 }
