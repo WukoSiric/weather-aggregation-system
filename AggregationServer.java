197a4110d6035eab2c2e55c6139f36d7e22bd7a7
Luke Siric Stainer <lstainer2002@gmail.com>
2023-10-10 00:51:08 +1030

    Aggregation server handles contentServer

--- AggregationServer.java
+++ AggregationServer.java
@@ -1,22 +1,66 @@
-import java.io.*; 
-import java.net.*; 
+import java.io.*;
+import java.net.*;
 
 public class AggregationServer {
-    private AggregationServer() {}
-    
     public static void main(String[] args) {
-        int port = args.length > 0 ? Integer.parseInt(args[0]) : 4567;
-        System.out.println("Starting server on port " + port);
-        
-        try {
-            ServerSocket ss = new ServerSocket(port); 
-            Socket s = ss.accept();
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            String str = (String)dis.readUTF();
-            System.out.println("Message: " + str);
-            ss.close(); 
-        } catch(Exception e) {
-            System.out.println("Error: " + e);
+        int defaultPort = 4567; // Default port number if no argument is provided
+        int port;
+
+        if (args.length > 0) {
+            // Use the provided port number if an argument is provided
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid port number. Using default port " + defaultPort);
+                port = defaultPort;
+            }
+        } else {
+            // Use the default port if no argument is provided
+            port = defaultPort;
+        }
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("AggregationServer listening on port " + port);
+
+            while (true) {
+                // Accept incoming client connections
+                Socket clientSocket = serverSocket.accept();
+
+                // Handle the connection in a separate thread
+                Thread clientThread = new ContentServerHandler(clientSocket);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static class ContentServerHandler extends Thread {
+        private final Socket clientSocket;
+
+        public ContentServerHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ) {
+                // Read and print the incoming PUT request
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                System.err.println("Error handling client request: " + e.getMessage());
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (IOException e) {
+                    System.err.println("Error closing client socket: " + e.getMessage());
+                }
+            }
         }
     }
 }
