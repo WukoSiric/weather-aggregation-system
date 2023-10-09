2513dec02a1141a351ccaafc845fb9a41a95d434
WukoSiric <lstainer2002@gmail.com>
2023-10-09 22:37:34 +1030

    Content Server connects with agg server

--- ContentServer.java
+++ ContentServer.java
@@ -5,18 +5,21 @@ public class ContentServer {
     private ContentServer() {}
 
     public static void main(String[] args) {
-        if (args.length != 1) { 
-            System.err.println("Usage: java ContentServer <port number>"); 
+        if (args.length != 2) { 
+            System.err.println("Usage: java ContentServer servername:portnumber file_path"); 
             System.exit(1); 
         }
-        int port = Integer.parseInt(args[0]);
         try {
-            Socket s = new Socket("localhost", port);
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.writeUTF("Hello, server!");
-            out.flush();
-            out.close();
-            s.close();
+            String[] hostname_port = URIParser.parse(args[0]);
+            String hostname = hostname_port[0];
+            int port = Integer.parseInt(hostname_port[1]);
+            System.out.println("Connecting to server " + hostname + " on port " + port);
+            // Socket s = new Socket("localhost", port);
+            // DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            // out.writeUTF("Hello, server!");
+            // out.flush();
+            // out.close();
+            // s.close();
         } catch(Exception e) {
             System.out.println("Error: " + e);
         }
