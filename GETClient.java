8af351ae3abee3a5054fa962604287c1a9d9f47e
Luke Siric Stainer <lstainer2002@gmail.com>
2023-10-10 23:07:42 +1030

    Parses arguments

--- GETClient.java
+++ GETClient.java
@@ -1,3 +1,21 @@
 public class GETClient {
-    
+    public static void main(String[] args) {
+        // Check arguments 
+        if (args.length < 1) {
+            System.err.println("Usage: java GETClient servername:portnumber stationID(optional)");
+            System.exit(1);
+        }
+
+        String[] hostname_port = URIParser.parse(args[0]);
+        String hostname = hostname_port[0];
+        int port = Integer.parseInt(hostname_port[1]);
+        String stationID = null;
+        
+        if (args.length == 2) {
+            stationID = args[1];
+        }
+
+        System.out.println("GETClient connecting to " + hostname + ":" + port);
+        System.out.println("StationiD: " + stationID); 
+    }
 }
