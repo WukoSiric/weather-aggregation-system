3f7735839baf1fccad4e5555cc99f304f3170d3d
Luke Siric Stainer <lstainer2002@gmail.com>
2023-10-10 23:07:16 +1030

    Read requests based off content length

--- AggregationServer.java
+++ AggregationServer.java
@@ -3,11 +3,10 @@ import java.net.*;
 
 public class AggregationServer {
     public static void main(String[] args) {
-        int defaultPort = 4567; // Default port number if no argument is provided
+        final int defaultPort = 4567; // Default port number if no argument is provided
         int port;
 
         if (args.length > 0) {
-            // Use the provided port number if an argument is provided
             try {
                 port = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
@@ -15,19 +14,16 @@ public class AggregationServer {
                 port = defaultPort;
             }
         } else {
-            // Use the default port if no argument is provided
             port = defaultPort;
         }
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("AggregationServer listening on port " + port);
-
             while (true) {
                 // Accept incoming client connections
                 Socket clientSocket = serverSocket.accept();
-
                 // Handle the connection in a separate thread
-                Thread clientThread = new ContentServerHandler(clientSocket);
+                Thread clientThread = new ClientHandler(clientSocket);
                 clientThread.start();
             }
         } catch (IOException e) {
@@ -35,10 +31,10 @@ public class AggregationServer {
         }
     }
 
-    private static class ContentServerHandler extends Thread {
+    private static class ClientHandler extends Thread {
         private final Socket clientSocket;
 
-        public ContentServerHandler(Socket clientSocket) {
+        public ClientHandler(Socket clientSocket) {
             this.clientSocket = clientSocket;
         }
 
@@ -46,11 +42,44 @@ public class AggregationServer {
         public void run() {
             try (
                 BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
             ) {
-                // Read and print the incoming PUT request
+                // Parse entire request including headers
+                StringBuilder request = new StringBuilder();
                 String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                    request.append(line).append("\n");
+                }
+
+                // Check if the request is a valid PUT request with required headers
+                if (isValidPutRequest(request.toString())) {
+                    // Extract content length
+                    int contentLength = extractContentLength(request.toString());
+
+                    // Read the request body based on content length
+                    StringBuilder requestBody = new StringBuilder();
+                    char[] buffer = new char[1024];
+                    int bytesRead;
+                    while (contentLength > 0 && (bytesRead = reader.read(buffer, 0, Math.min(contentLength, buffer.length))) != -1) {
+                        requestBody.append(buffer, 0, bytesRead);
+                        contentLength -= bytesRead;
+                    }
+
+                    // Now you can process the request body in 'requestBody' variable
+                    System.out.println("Received valid PUT request:\n" + request);
+                    System.out.println("Request Body:\n" + requestBody.toString());
+
+                    // Respond with a 200 OK
+                    writer.write("HTTP/1.1 200 OK\r\n");
+                    writer.write("Content-Length: 0\r\n");
+                    writer.write("\r\n");
+                    writer.flush();
+                } else {
+                    // Respond with a 400 Bad Request
+                    writer.write("HTTP/1.1 400 Bad Request\r\n");
+                    writer.write("Content-Length: 0\r\n");
+                    writer.write("\r\n");
+                    writer.flush();
                 }
             } catch (IOException e) {
                 System.err.println("Error handling client request: " + e.getMessage());
@@ -61,6 +90,29 @@ public class AggregationServer {
                     System.err.println("Error closing client socket: " + e.getMessage());
                 }
             }
+        }
+
+        private static boolean isValidPutRequest(String request) {
+            // Check if the request is a valid PUT request with required headers
+            return 
+                    request.startsWith("PUT") &&
+                    request.contains("Content-Type: application/json") &&
+                    request.contains("Content-Length: ");
+        }
+
+        private static int extractContentLength(String request) {
+            // Extract and parse the "Content-Length" header value
+            int startIndex = request.indexOf("Content-Length: ");
+            if (startIndex != -1) {
+                int endIndex = request.indexOf("\n", startIndex);
+                String lengthStr = request.substring(startIndex + "Content-Length: ".length(), endIndex).trim();
+                try {
+                    return Integer.parseInt(lengthStr);
+                } catch (NumberFormatException e) {
+                    // Handle parsing error if necessary
+                }
+            }
+            return -1; // Return -1 if header is not found or parsing fails
         }
     }
 }
