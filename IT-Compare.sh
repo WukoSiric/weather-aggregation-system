b7ddce5201115a3b5aa213bbbcc85f67281c7c41
Luke Siric Stainer <lstainer2002@gmail.com>
2023-10-13 05:57:11 +1030

    Give overall result for integration tests

--- IT-Compare.sh
+++ IT-Compare.sh
@@ -6,6 +6,9 @@ LOG_DIR="./Tests"
 # Find all log files in the specified directory
 LOG_FILES=($(find "$LOG_DIR" -type f -name "*.log"))
 
+# Initialize a variable to track overall result
+overall_result="Passed"
+
 # Iterate over the log files
 for log_file in "${LOG_FILES[@]}"; do
     comp_file="${log_file}.comp"
@@ -13,12 +16,17 @@ for log_file in "${LOG_FILES[@]}"; do
         checksum_comp=$(md5sum "$comp_file" | awk '{print $1}')
         checksum_log=$(md5sum "$log_file" | awk '{print $1}')
         echo "Comparing checksums for $comp_file and $log_file:"
-    if [ "$checksum_comp" == "$checksum_log" ]; then
-        echo "Checksums match."
-    else
-        echo "Checksums do not match."
-    fi
+        if [ "$checksum_comp" == "$checksum_log" ]; then
+            echo "Checksums match. Passed"
+        else
+            echo "Checksums do not match. Failed"
+            overall_result="Failed"
+        fi
     else
         echo "One or both files not found: $comp_file, $log_file"
+        overall_result="Failed"
     fi
 done
+
+# Print overall result
+echo "Overall Result: $overall_result"
