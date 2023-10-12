28d973dc5cb2b9fea5320cd2f16915afe6afb3c2
WukoSiric <lstainer2002@gmail.com>
2023-10-13 00:18:12 +1030

    Attempts connection multiple times

--- ContentServer.java
+++ ContentServer.java
@@ -7,17 +7,18 @@ public class ContentServer {
     private ContentServer() {}
 
     public static void main(String[] args) {
-        if (args.length != 2) { 
-            System.err.println("Usage: java ContentServer servername:portnumber file_path"); 
-            System.exit(1); 
+        if (args.length != 2) {
+            System.err.println("Usage: java ContentServer servername:portnumber file_path");
+            System.exit(1);
         }
+    
         try {
-            // Parse the servername, port and file path from the command line
+            // Parse the servername, port, and file path from the command line
             String[] hostname_port = URIParser.parse(args[0]);
             String hostname = hostname_port[0];
             int port = Integer.parseInt(hostname_port[1]);
             String file_path = args[1];
-            
+    
             // Read the file
             ContentServer server = new ContentServer();
             JSONObject json = server.readFile(file_path);
@@ -25,35 +26,68 @@ public class ContentServer {
                 System.out.println("Error: File not found");
                 System.exit(1);
             }
-
+    
             // Construct put request
             String request = server.constructPUTRequest(json);
-
-            //  Establish connection with server
-            Socket socket = new Socket(hostname, port);
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            // Send request to server
-            writer.write(request);
-            writer.flush();
-
-            // Read response from server
-            String line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
+    
+            int maxRetryAttempts = 3; // Set your desired maximum retry attempts
+            int retryCount = 0;
+    
+            boolean success = false;
+    
+            while (retryCount < maxRetryAttempts) {
+                try {
+                    // Establish connection with the server
+                    Socket socket = new Socket(hostname, port);
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    
+                    // Send request to server
+                    writer.write(request);
+                    writer.flush();
+    
+                    // Read response from server
+                    String line = reader.readLine();
+                    StringBuilder responseBuilder = new StringBuilder();
+                    while (line != null) {
+                        System.out.println(line);
+                        responseBuilder.append(line);
+                        line = reader.readLine();
+                    }
+    
+                    // Close the connection
+                    socket.close();
+    
+                    // Check if the operation was successful
+                    if (checkIfSuccessful(responseBuilder.toString())) {
+                        success = true;
+                        break;
+                    }
+                } catch (IOException e) {
+                    // Handle connection errors, e.g., connection refused
+                    System.out.println("Error connecting to the server: " + e.getMessage());
+                }
+    
+                retryCount++;
+                Thread.sleep(1000); // Wait for a moment before retrying
             }
-
-            // Close the connection
-            socket.close();
-        } catch(Exception e) {
+    
+            if (!success) {
+                System.out.println("Failed to connect after " + maxRetryAttempts + " attempts.");
+            }
+        } catch (Exception e) {
             System.out.println("Error: " + e);
         }
     }
-
+    
+    private static boolean checkIfSuccessful(String response) {
+        if (response.startsWith("HTTP/1.1 2")) {
+            return true;
+        }
+        return false; 
+    }
+    
     public JSONObject readFile(String file_path) {
-        // Read the file and return a JSONObject
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader(file_path));
@@ -69,7 +103,6 @@ public class ContentServer {
             System.out.println("Error: " + e);
         }
 
-        // Return null if there is an error
         return null;
     }
 
